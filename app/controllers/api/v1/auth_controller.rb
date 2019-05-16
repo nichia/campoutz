b350e45316228da0a65f22dc331f91af04d0ce9f
nichia <nichia@gmail.com>
2019-05-16 14:18:39 -0400

    Update Rails api to return errors on failed requests

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -11,7 +11,7 @@ class Api::V1::AuthController < ApplicationController
       @token = encode_token(payload)
       render json: { user: UserSerializer.new(@user), jwt: @token }, status: :accepted
     else
-      render json: { message: 'Invalid username or password' }, status: :unauthorized
+      render json: { errors: ['Invalid username or password'] }, status: :unauthorized
     end
   end
 
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::UsersController < ApplicationController
 
   # http://api.rubyonrails.org/classes/ActionController/ParamsWrapper.html
   # Don't know why: with rails --api, json format - cannot receive params user 'password' because it is not an attribute/column in User model (User has password_digest). Need to add this wrap_parameters to fix the issue.
-  wrap_parameters :user, include: %i[username password email firstname lastname bio avatar]
+  # wrap_parameters :user, include: %i[username password email firstname lastname bio avatar]
 
   # An authenticated user can access their profile information
   def profile
@@ -12,13 +12,14 @@ class Api::V1::UsersController < ApplicationController
 
   def create
     # raise params.inspect
-    # byebug
     @user = User.create(user_params)
+    byebug
     if @user.valid?
       @token = encode_token(user_id: @user.id)
       render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
     else
-      render json: { error: 'failed to create user' }, status: :not_acceptable
+      errors = @user.errors.full_messages
+      render json: {errors: errors}, status: :not_acceptable
     end
   end
 
