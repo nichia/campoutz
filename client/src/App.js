4d6cdd3f5a09eac46dc10c68108523d23fafd9d3
nichia <nichia@gmail.com>
2019-05-19 17:49:02 -0400

    Remove default react create app codes

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 import React, { Fragment } from 'react';
 import { Route, Switch, Redirect, withRouter } from 'react-router-dom';
+
 import Navbar from './components/navbar';
 import LoginForm from './components/loginForm';
 import SignupForm from './components/signupForm';
 import NoMatch from './components/noMatch';
 import Profile from './components/profile';
 
-// import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -14,30 +14,12 @@ function App() {
     <Fragment>
       <Navbar />
       <Switch>
-        <Route exact path='/' render={() => <Redirect to='/profile' />} />
-        <Route exact path='/profile' component={Profile} />
-        <Route exact path='/login' component={LoginForm} />
-        <Route exact path='/signup' component={SignupForm} />
+        <Route exact path="/" render={() => <Redirect to="/profile" />} />
+        <Route exact path="/profile" component={Profile} />
+        <Route exact path="/login" component={LoginForm} />
+        <Route exact path="/signup" component={SignupForm} />
         <Route component={NoMatch} />
       </Switch>
-
-      {/* <div className='App'>App Container</div>
-      <div className='App'>
-        <header className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className='App-link'
-            href='https://reactjs.org'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            Learn React
-          </a>
-        </header>
-      </div> */}
     </Fragment>
   );
 }
