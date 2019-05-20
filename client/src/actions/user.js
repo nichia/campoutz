9ac88e5773adb6f15a6861a23cecfa7127a08bdf
nichia <nichia@gmail.com>
2019-05-19 21:46:42 -0400

    Refactor user actions

--- client/src/actions/user.js
+++ client/src/actions/user.js
@@ -1,152 +1,8 @@
 import * as actionTypes from './actionTypes';
 
-// async action createors
+const baseUrl = `${process.env.REACT_APP_API_ENDPOINT}/api/v1`;
 
-export const /*FUNCTION*/ signupUser = (username, email, password) => {
-    return /*FUNCTION*/ dispatch => {
-      //thunk
-      console.log(
-        '%c signupUser: ',
-        'color: navy',
-        process.env.REACT_APP_API_ENDPOINT
-      );
-      // })
-      // dispatch({ type: actionTypes.AUTHENTICATING_USER });
-      dispatch(authenticatingUser());
-      // fetch(`${process.env.REACT_APP_API_ENDPOINT}/api/v1/users`)
-      // adapter.signupUser(username, password)
-      // http://localhost:3000
-      fetch(`${process.env.REACT_APP_API_ENDPOINT}/api/v1/users`, {
-        //TODO: move this to an adapter
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          Accept: 'application/json'
-        },
-        body: JSON.stringify({
-          user: {
-            username: username,
-            email: email,
-            password: password
-          }
-        })
-      })
-        .then(response => {
-          if (response.ok) {
-            return response.json();
-          } else {
-            throw response;
-          }
-        })
-        /* { user:
-          { username: 'chandler bing', bio: '', avatar: ''},
-          jwt: 'aaaaaaaaaaaaaaa.bbbbbbbbbbbbbbbbbbbbb.ccccccccccccccccccc'
-      } */
-        .then(JSONResponse => {
-          console.log('%c INSIDE YE OLDE .THEN', 'color: navy');
-          localStorage.setItem('jwt', JSONResponse.jwt);
-          dispatch({
-            type: actionTypes.SET_CURRENT_USER,
-            payload: JSONResponse.user
-          });
-          // dispatch(setCurrentUser(JSONResponse.user))
-        })
-        .catch(r =>
-          r
-            .json()
-            .then(e =>
-              dispatch({ type: actionTypes.FAILED_LOGIN, payload: e.message })
-            )
-        );
-      // .then((jsonResponse) => {
-      //   localStorage.setItem('jwt', jsonResponse.jwt)
-      //   dispatch(setCurrentUser(jsonResponse.user))
-      // })
-    };
-  };
-
-export const /*FUNCTION*/ loginUser = (username, password) => {
-    return /*FUNCTION*/ dispatch => {
-      //thunk
-      console.log(
-        '%c loginUser: ',
-        'color: navy',
-        process.env.REACT_APP_API_ENDPOINT
-      );
-      // dispatch({ type: actionTypes.AUTHENTICATING_USER });
-      // dispatch(authenticatingUser())
-      // fetch(`${process.env.REACT_APP_API_ENDPOINT}/api/v1/login`)
-      // adapter.loginUser(username, password)
-      // http://localhost:3000
-      fetch(`${process.env.REACT_APP_API_ENDPOINT}/api/v1/login`, {
-        //TODO: move this to an adapter
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          Accept: 'application/json'
-        },
-        body: JSON.stringify({
-          user: {
-            username: username,
-            password: password
-          }
-        })
-      })
-        .then(response => {
-          if (response.ok) {
-            return response.json();
-          } else {
-            throw response;
-          }
-        })
-        /* { user:
-    { username: 'chandler bing', bio: '', avatar: ''},
-    jwt: 'aaaaaaaaaaaaaaa.bbbbbbbbbbbbbbbbbbbbb.ccccccccccccccccccc'
-  } */
-        .then(JSONResponse => {
-          console.log('%c INSIDE YE OLDE .THEN', 'color: navy');
-          localStorage.setItem('jwt', JSONResponse.jwt);
-          dispatch({
-            type: actionTypes.SET_CURRENT_USER,
-            payload: JSONResponse.user
-          });
-          // dispatch(setCurrentUser(JSONResponse.user))
-        })
-        .catch(r =>
-          r
-            .json()
-            .then(e =>
-              dispatch({ type: actionTypes.FAILED_LOGIN, payload: e.message })
-            )
-        );
-      // .then((jsonResponse) => {
-      //   localStorage.setItem('jwt', jsonResponse.jwt)
-      //   dispatch(setCurrentUser(jsonResponse.user))
-      // })
-    };
-  };
-
-export const fetchCurrentUser = () => {
-  // takes the token in localStorage and finds out who it belongs to
-  return dispatch => {
-    console.log(
-      '%c fetchCurrentUser: ',
-      'color: navy',
-      process.env.REACT_APP_API_ENDPOINT
-    );
-    dispatch(authenticatingUser()); //tells the app we are fetching
-    fetch(`${process.env.REACT_APP_API_ENDPOINT}/api/v1/profile`, {
-      method: 'GET',
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem('jwt')}`
-      }
-    })
-      .then(response => response.json())
-      .then(JSONResponse => dispatch(setCurrentUser(JSONResponse.user)));
-  };
-};
-
-// action creators
+// action creators //
 
 export const setCurrentUser = userData => ({
   type: actionTypes.SET_CURRENT_USER,
@@ -168,4 +24,140 @@ export const logoutUser = () => {
   console.log('%c INSIDE LOGOUT action/user', 'color: navy');
   localStorage.removeItem('jwt');
   return { type: actionTypes.LOGOUT };
+};
+
+// async action createors //
+
+export const /*FUNCTION*/ signupUser = (username, email, password) => {
+    const data = {
+      //TODO: move this to an adapter
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+      },
+      body: JSON.stringify({
+        user: {
+          username: username,
+          email: email,
+          password: password
+        }
+      })
+    };
+
+    return /*FUNCTION*/ dispatch => {
+      //thunk
+      console.log(
+        '%c signupUser: ',
+        'color: navy',
+        process.env.REACT_APP_API_ENDPOINT
+      );
+
+      // fetch(`${process.env.REACT_APP_API_ENDPOINT}/api/v1/users`)
+      // adapter.signupUser(username, email, password)
+      // http://localhost:3000
+
+      fetch(`${baseUrl}/users`, data)
+        .then(response => {
+          if (response.ok) {
+            return response.json();
+          } else {
+            throw response;
+          }
+        })
+        /* { user: { username: 'chandler bing', bio: '', avatar: ''}, jwt: 'aaaaaaaaaaaaaaa.bbbbbbbbbbbbbbbbbbbbb.ccccccccccccccccccc'} */
+        .then(JSONResponse => {
+          console.log('%c INSIDE YE OLDE .THEN', 'color: navy');
+          localStorage.setItem('jwt', JSONResponse.jwt);
+          dispatch(setCurrentUser(JSONResponse.user));
+          // dispatch({
+          //   type: actionTypes.SET_CURRENT_USER,
+          //   payload: JSONResponse.user
+          // });
+        })
+        .catch(res =>
+          res.json().then(
+            err => dispatch(failedLogin(err.message))
+            // dispatch({ type: actionTypes.FAILED_LOGIN, payload: err.message })
+          )
+        );
+    };
+  };
+
+export const /*FUNCTION*/ loginUser = (username, password) => {
+    const data = {
+      //TODO: move this to an adapter
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+      },
+      body: JSON.stringify({
+        user: {
+          username: username,
+          password: password
+        }
+      })
+    };
+
+    return /*FUNCTION*/ dispatch => {
+      //thunk
+      console.log(
+        '%c loginUser: ',
+        'color: navy',
+        process.env.REACT_APP_API_ENDPOINT
+      );
+      // dispatch({ type: actionTypes.AUTHENTICATING_USER });
+      dispatch(authenticatingUser());
+
+      // fetch(`${process.env.REACT_APP_API_ENDPOINT}/api/v1/login`)
+      // adapter.loginUser(username, password)
+      // http://localhost:3000
+      fetch(`${baseUrl}/login`, data)
+        .then(response => {
+          if (response.ok) {
+            return response.json();
+          } else {
+            throw response;
+          }
+        })
+        .then(JSONResponse => {
+          console.log('%c INSIDE YE OLDE .THEN', 'color: navy');
+          /* { user: { username: 'chandler bing', bio: '', avatar: ''}, jwt: 'aaaaaaaaaaaaaaa.bbbbbbbbbbbbbbbbbbbbb.ccccccccccccccccccc'} */
+          localStorage.setItem('jwt', JSONResponse.jwt);
+          dispatch(setCurrentUser(JSONResponse.user));
+          // dispatch({
+          //   type: actionTypes.SET_CURRENT_USER,
+          //   payload: JSONResponse.user
+          // });
+        })
+        .catch(res =>
+          res.json().then(
+            err => dispatch(failedLogin(err.message))
+            // dispatch({ type: actionTypes.FAILED_LOGIN, payload: err.message })
+          )
+        );
+    };
+  };
+
+export const fetchCurrentUser = () => {
+  // takes the token in localStorage and finds out who it belongs to
+  const data = {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem('jwt')}`
+    }
+  };
+
+  return dispatch => {
+    console.log(
+      '%c fetchCurrentUser: ',
+      'color: navy',
+      process.env.REACT_APP_API_ENDPOINT
+    );
+    dispatch(authenticatingUser()); //tells the app we are fetching
+    fetch(`${baseUrl}/profile`, data)
+      .then(response => response.json())
+      .then(JSONResponse => dispatch(setCurrentUser(JSONResponse.user)));
+  };
 };
