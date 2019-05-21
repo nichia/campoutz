61a37102868f591a106d03c7b9a5ea9715195a6e
nichia <nichia@gmail.com>
2019-05-21 14:42:01 -0400

    Change the page when invalid route is entered

--- client/src/components/noMatch.js
+++ client/src/components/noMatch.js
@@ -1,13 +1,41 @@
 import React, { Fragment } from 'react';
-import { Header, Image } from 'semantic-ui-react';
+import errorBgImage from '../assets/images/error-bg-image.jpg';
+import { Header, Icon } from 'semantic-ui-react';
 
 const NoMatch = () => (
-  <Fragment>
-    <Header size='huge' inverted color='purple'>
-      Page Not Found lol
+  <div>
+    <style>
+      {`
+        html, body{
+          background-image:     linear-gradient(
+            rgba(0, 0, 0, 0.5),
+            rgba(0, 0, 0, 0.5)
+          ),url(${errorBgImage});
+          width: 100vw;
+          height: 100vh; 
+          background-repeat: no-repeat;
+          background-size: cover;
+          position: relative;
+        }
+
+        white-text{
+          color: white;
+        }
+      `}
+    </style>
+    <Header as='h2' icon textAlign='center'>
+      <white-text>
+        <Icon name='settings' />
+        Please Bear With Us
+      </white-text>
+      <Header.Subheader textAlign='center'>
+        <white-text>
+          Looks like we got lost on the trail and can't seem to find the path
+          you're looking for.
+        </white-text>
+      </Header.Subheader>
     </Header>
-    <Image src='https://thumbs.dreamstime.com/z/senior-man-shrugging-his-shoulders-8775727.jpg' />
-  </Fragment>
+  </div>
 );
 
 export default NoMatch;
