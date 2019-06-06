efa9d49c2fbe5f15f7ccbad959df3237dcc4fb58
nichia <nichia@gmail.com>
2019-06-06 17:24:19 -0400

    Change CampgroundsCards to display description overview only

--- client/src/components/CampgroundsCards.js
+++ client/src/components/CampgroundsCards.js
@@ -2,6 +2,29 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { Card, Icon } from 'semantic-ui-react';
 
+/* fix(card): word-wrap overflowing card container with longstrings */
+const divStyle = {
+  wordWrap: 'break-word'
+};
+
+const extractDescription = description => {
+  let extractedDescription = description;
+  let indexStart = description.indexOf('<p>');
+  let indexEnd = description.indexOf('</p>');
+  if (indexEnd > indexStart) {
+    extractedDescription = description.substring(indexStart + 3, indexEnd);
+  } else {
+    indexStart = description.indexOf('</h2>');
+    if (indexStart !== '-1') {
+      indexEnd = description.indexOf('<h2>', indexStart + 1);
+      if (indexEnd > indexStart) {
+        extractedDescription = description.substring(indexStart + 5, indexEnd);
+      }
+    }
+  }
+  return extractedDescription;
+};
+
 const CampgroundsView = ({ children }) => {
   console.log('%c CampgroundsView ', 'color: green', children);
 
@@ -15,15 +38,23 @@ const CampgroundsView = ({ children }) => {
           <Card.Content>
             {/* fix(card): word-wrap overflowing card container with long
                     strings */}
-            <div className='description' Style='word-wrap: break-word;'>
-              <Card.Description>
-                <div
-                  dangerouslySetInnerHTML={{
-                    __html: campground.FacilityDescription
-                  }}
-                />
-              </Card.Description>
-            </div>
+            {/* <div
+              className='description'
+              Style='width: 200px; white-space: nowrape; overflow: hidden; text-overflow: ellipsis;'
+            > */}
+            {/* <div className='description' Style='word-wrap: break-word;'> */}
+            {/* <Card.Description Style='width: 200px; white-space: nowrape; overflow: hidden; text-overflow: ellipsis;'> */}
+            <Card.Description>
+              <div
+                style={divStyle}
+                dangerouslySetInnerHTML={{
+                  __html: extractDescription(campground.FacilityDescription)
+                }}
+              />
+              {/* // campground.FacilityDescription.substring(0, 100) + '...' // */}
+              {/* `${campground.FacilityDescription.substring(0, 100)}...` */}
+            </Card.Description>
+            {/* </div> */}
           </Card.Content>
           <Card.Content extra>
             <Icon name='user' />4 Friends
