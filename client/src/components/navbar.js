5b9b057e920a4e788fe27a53c10a6a66531ae390
nichia <nichia@gmail.com>
2019-06-16 15:29:06 -0400

    Refactor navbar page

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,44 +1,113 @@
-import React, { Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
-import { NavLink, withRouter } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 import { Menu } from 'semantic-ui-react';
 import { logoutUser } from '../actions/userActions';
 
-const Navbar = ({ logoutUser, user: { loggedIn }, location: { pathname } }) => {
-  return (
-    <Menu pointing secondary>
-      {loggedIn ? (
-        <Fragment>
+class Navbar extends Component {
+  state = { activeItem: 'campoutz' };
+
+  // handleItemClick = (e, { name }) => this.setState({ activeItem: name });
+  handleItemClick = (e, { name, url }) => {
+    this.setState({ activeItem: name });
+    this.props.history.push(url);
+  };
+  handleClickLogout = (e, { name, url }) => {
+    this.setState({ activeItem: name });
+    this.props.logoutUser(); //comes from mapDispatchToProps
+    this.props.history.push(url);
+  };
+
+  render() {
+    const { activeItem } = this.state;
+    const {
+      user: { loggedIn },
+      location: { pathname }
+    } = this.props;
+    return (
+      <div>
+        <Menu pointing secondary>
           <Menu.Item
-            as={NavLink}
-            to='/profile'
-            name='Profile'
-            active={pathname === '/profile'}
+            name='campoutz'
+            active={activeItem === 'campoutz'}
+            url='/'
+            onClick={this.handleItemClick}
           />
-          <Menu.Menu position='right'>
-            <Menu.Item
-              as={NavLink}
-              to='/'
-              name='Logout'
-              active={pathname === '/'}
-              onClick={() => {
-                logoutUser(); //comes from mapDispatchToProps
-              }}
-            />
-          </Menu.Menu>
-        </Fragment>
-      ) : (
-        <Menu.Menu position='right'>
-          {pathname === '/login' ? (
-            <Menu.Item as={NavLink} to='/signup' name='Signup' />
+          {loggedIn ? (
+            <Fragment>
+              <Menu.Item
+                name='profile'
+                active={activeItem === 'profile'}
+                url='/profile'
+                onClick={this.handleItemClick}
+              />
+              <Menu.Menu position='right'>
+                <Menu.Item
+                  name='logout'
+                  active={activeItem === 'logout'}
+                  url='/'
+                  onClick={this.handleClickLogout}
+                />
+              </Menu.Menu>
+            </Fragment>
           ) : (
-            <Menu.Item as={NavLink} to='/login' name='Login' />
+            <Menu.Menu position='right'>
+              {pathname === '/login' ? (
+                <Menu.Item
+                  name='signup'
+                  active={activeItem === 'signup'}
+                  url='/signup'
+                  onClick={this.handleItemClick}
+                />
+              ) : (
+                <Menu.Item
+                  name='login'
+                  active={activeItem === 'login'}
+                  url='login'
+                  onClick={this.handleItemClick}
+                />
+              )}
+            </Menu.Menu>
           )}
-        </Menu.Menu>
-      )}
-    </Menu>
-  );
-};
+        </Menu>
+      </div>
+    );
+  }
+}
+// const Navbar = ({ logoutUser, user: { loggedIn }, location: { pathname } }) => {
+//   return (
+//     <Menu pointing secondary>
+//       {loggedIn ? (
+//         <Fragment>
+//           <Menu.Item
+//             as={NavLink}
+//             to='/profile'
+//             name='Profile'
+//             active={pathname === '/profile'}
+//           />
+//           <Menu.Menu position='right'>
+//             <Menu.Item
+//               as={NavLink}
+//               to='/'
+//               name='Logout'
+//               onClick={() => {
+//                 logoutUser(); //comes from mapDispatchToProps
+//               }}
+//             />
+//           </Menu.Menu>
+//         </Fragment>
+//       ) : (
+//         <Menu.Menu position='right'>
+//           {pathname === '/login' ? (
+//             <Menu.Item as={NavLink} to='/signup' name='Signup' />
+//           ) : (
+//             <Menu.Item as={NavLink} to='/login' name='Login' />
+//           )}
+//         </Menu.Menu>
+//       )}
+//     </Menu>
+//   );
+// };
 
 const mapStateToProps = ({ user }) => ({ user });
 
