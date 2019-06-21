692b3a7c8fb7b6a51671e353c151171f0814350e
nichia <nichia@gmail.com>
2019-06-21 11:30:27 -0400

    Add dotenv to allow heroku access to .env via process.env variable

--- client/src/index.js
+++ client/src/index.js
@@ -5,9 +5,9 @@ import './index.css';
 import App from './App';
 import store from './store';
 import * as serviceWorker from './serviceWorker';
-
 import { Provider } from 'react-redux';
 import 'semantic-ui-css/semantic.min.css';
+require('dotenv').config();
 
 ReactDOM.render(
   <Provider store={store}>
